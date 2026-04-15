82bbd89257d6b97d5037e2fde0592dfea56f48a0
KEINOS <github@keinos.com>
2026-04-16 04:24:42 +0900

    chore: update module

--- .github/run_go_mod_tidy.sh
+++ .github/run_go_mod_tidy.sh
@@ -2,11 +2,6 @@
 # =============================================================================
 #  This script updates Go modules to the latest version.
 # =============================================================================
-#  It will remove the go.mod file and run `go mod tidy` to get the latest moule
-#  versions.
-#  Then it will run the tests to make sure the code is still working, and fails
-#  if any errors are found during the process.
-#
 #  NOTE: This script is aimed to run in the container via docker-compose.
 #    See "tidy" service: ./docker-compose.yml
 # =============================================================================
@@ -18,6 +13,7 @@ cp go.mod go.mod.bak
 cp go.sum go.sum.bak
 
 echo '* Run go tidy ...'
+go get -u ./...
 go mod tidy -go=1.25.0
 
 echo '* Run tests ...'
--- mdspec/_updater/go.mod
+++ mdspec/_updater/go.mod
@@ -3,15 +3,15 @@ module md-spec-check/download
 go 1.25.7
 
 require (
-	github.com/PuerkitoBio/goquery v1.11.0
+	github.com/PuerkitoBio/goquery v1.12.0
 	github.com/pkg/errors v0.9.1
 	github.com/zeebo/xxh3 v1.1.0
-	golang.org/x/mod v0.33.0
+	golang.org/x/mod v0.35.0
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.3 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	golang.org/x/net v0.53.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
 )
